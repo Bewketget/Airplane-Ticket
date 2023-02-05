c541943d49eaee6a779d3704c58ece881c0030b4
Bewketget <103607312+Bewketget@users.noreply.github.com>
2023-02-05 13:34:50 +0300

    initial commit

---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,20 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'hotel.png',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'hotel1.jpeg',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': 40
+  },
+  {
+    'image': 'hotel2.jpeg',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': 68
+  },
+];
